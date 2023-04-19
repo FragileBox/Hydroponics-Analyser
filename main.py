1bb766dc25e68d19d843cdcb8d8c635fbffe2b9d
FragileBox <109564180+FragileBox@users.noreply.github.com>
2023-04-19 16:49:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,9 @@ import streamlit as st
 
 st.title("Hydroponics Analyser")
 
-mass = st.number_input("What is your mass(kg): ", step = 0.1)
-height = st.number_input("What is your height(m): ", step = 0.01)
+mass = st.number("What is your mass(kg): ", step = 100)
+height = st.slider("What is your height(m): ", step = 100)
 
-button = st.button("Calculate BMI")
+button = st.button("Predict Conditions")
 if button:
-  BMI = mass/height**2
-  if BMI <= 18.4:
-    st.success("You are Risk of Nutrional Deficiency.")
-  elif BMI <= 22.9:
-    st.success("You are Low Risk.")
-  elif BMI <= 27.4:
-    st.success("You are in Moderate Risk.")
-  else:
-    st.success("You are in High Risk.")
+    st.success("Analysing data...")

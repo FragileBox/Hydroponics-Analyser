bf8995bd873e213a8ab27c812032bf5e544f7f39
FragileBox <109564180+FragileBox@users.noreply.github.com>
2023-04-20 11:27:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ new_sol = st.number_input("Nutrient solution to be added(kg): ", step = 0.01)
 add_sol = st.number_input("Nutrient solution already added(kg): ", step = 0.01)
 Sodium = st.slider("Na content(mg): ", step = 1, min_value = 400, max_value = 2100)
 Potassium = st.slider("K content(mg): ", step = 1, min_value = 400, max_value = 2100)
-Magnessium = st.slider("Mg content(mg): ", step = 1, min_value = 400, max_value = 2100)
+Magnesium = st.slider("Mg content(mg): ", step = 1, min_value = 400, max_value = 2100)
 Calcium = st.slider("Ca content(mg): ", step = 1, min_value = 400, max_value = 2100)
 
 button = st.button("Predict Conditions")
